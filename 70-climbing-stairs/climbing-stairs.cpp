16793fffe9ac645b6c504c8bd8d664ea00583623
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2025-12-26 09:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -8,15 +8,16 @@ public:
     //     return(dp[n]);
     // }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
         if(n==1)    return 1;
         if(n==2)    return 2;
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+        int prev=1;
+        int curr=2;
+        int next;
         for(int i=3;i<n+1;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            next=curr+prev;
+            prev=curr;
+            curr=next;
         }
-        return(dp[n]);
+        return(next);
     }
 };
