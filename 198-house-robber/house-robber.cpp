f79fafc967d3a6144fbada7bd47b1da6be76d284
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2025-12-26 22:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.09%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    // int helper(int idx, vector<int>& nums, int maxi, vector<int>& dp){
-    //     if(idx==0){
-    //         //dp[0]=nums[0];
-    //         return nums[0];
-    //     }  
-    //     if(idx<0)   return 0;
-    //     if(dp[idx]==-1){
-    //         int pick=nums[idx]+helper(idx-2, nums, INT_MIN, dp);
-    //         int not_pick=helper(idx-1, nums, INT_MIN, dp);
-    //         dp[idx]=max(pick,not_pick);
-    //         return(dp[idx]);
-    //     }
-    //     return dp[idx];
-    // }
+    int solver(int n, vector<int>& nums,vector<int> &dp){
+        if(n==0)    return 0;
+        if(n==1)    return nums[n-1];
+        int take=0,nottake=0;
+        if(dp[n]!=-1)   return dp[n];
+        take=nums[n-1]+solver(n-2,nums,dp);
+        nottake=solver(n-1,nums,dp);
+        return(dp[n]=max(take,nottake));
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        //vector<int> dp(n,-1);
-        if(n==1)    return nums[0];
-        if(n==2)    return(max(nums[0],nums[1]));
-        int prev=nums[0], val;
-        int curr=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            int pick=nums[i]+prev;
-            int not_pick=curr;
-            val=max(pick,not_pick);
-            prev=curr;
-            curr=val;
-        }
-        return(val);
+        vector<int> dp(n+1,-1);
+        return(solver(n,nums,dp));
     }
 };
