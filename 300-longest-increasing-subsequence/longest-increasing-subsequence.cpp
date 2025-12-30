5f6fb156e4fa5b636795fc56b07d4950fb62c284
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2025-12-30 11:32:57 +0530

    Time: 256 ms (33.27%) | Memory: 296.6 MB (11.44%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int solver(int idx, vector<int>& nums, int n, int prev_idx,vector<vector<int>> &dp){
-        if(idx==n) return 0;
-        if(dp[idx][prev_idx+1]!=-1) return dp[idx][prev_idx+1];
-        dp[idx][prev_idx+1]=solver(idx+1,nums,n,prev_idx,dp);
-        if(prev_idx==-1||nums[idx]>nums[prev_idx]){
-            dp[idx][prev_idx+1] = max(dp[idx][prev_idx+1], 1+(solver(idx+1,nums,n,idx,dp)));
-        }
-        return dp[idx][prev_idx+1];
-    }
+    // int solver(int idx, vector<int>& nums, int n, int prev_idx,vector<vector<int>> &dp){
+    //     if(idx==n) return 0;
+    //     if(dp[idx][prev_idx+1]!=-1) return dp[idx][prev_idx+1];
+    //     dp[idx][prev_idx+1]=solver(idx+1,nums,n,prev_idx,dp);//not taking
+    //     if(prev_idx==-1||nums[idx]>nums[prev_idx]){
+    //         dp[idx][prev_idx+1] = max(dp[idx][prev_idx+1], 1+(solver(idx+1,nums,n,idx,dp)));
+    //     }
+    //     return dp[idx][prev_idx+1];
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        return solver(0,nums,n,-1,dp);
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        for(int idx=n-1;idx>-1;idx--){
+            for(int prev_idx=idx-1;prev_idx>=-1;prev_idx--){
+                dp[idx][prev_idx+1]=dp[idx+1][prev_idx+1];
+                if(prev_idx==-1||nums[idx]>nums[prev_idx]){
+                    dp[idx][prev_idx+1] = max(dp[idx+1][prev_idx+1], 1+dp[idx+1][idx+1]);
+                }
+            }   
+        }
+        return dp[0][0];
     }
 };
