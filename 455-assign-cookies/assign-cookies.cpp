b7185c37f4607551f259103e8a51e2384930ed0f
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-01-03 08:48:34 +0530

    Time: 111 ms (6.30%) | Memory: 46 MB (10.24%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -3,19 +3,30 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int n=g.size(),m=s.size();
         vector<int> v(m,-1);
-        sort(g.rbegin(),g.rend());
-        sort(s.rbegin(),s.rend());
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(s[j]>=g[i] && v[j]==-1){
-                    v[j]=0;
-                    c++;
-                    break;
-                }
-                if(g[i]>s[j])   break;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int c=0,l=0,r=0;
+        while(l<=m-1 && r<=n-1){
+            if(s[l]>=g[r]){
+                cout<<l<<" "<<r<<endl;
+                l++;
+                r++;
+                c++;
+                continue;
             }
+            l++;
         }
         return c;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(s[j]>=g[i] && v[j]==-1){
+        //             v[j]=0;
+        //             c++;
+        //             break;
+        //         }
+        //         if(g[i]>s[j])   break;
+        //     }
+        // }
+        // return c;
     }
 };
