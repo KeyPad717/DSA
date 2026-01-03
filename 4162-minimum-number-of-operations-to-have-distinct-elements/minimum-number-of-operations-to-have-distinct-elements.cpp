8a08a211bc0fe5cf86e94b5ccee7e1be6db5c9be
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-01-03 18:48:49 +0530

    Time: 107 ms (55.57%) | Memory: 187.5 MB (61.06%) - LeetSync

--- 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -6,15 +6,12 @@ public:
         int ind=0,flag=0;
         for(int i=n-1;i>=0;i--){
             mp[nums[i]]++;
-            cout<<"ele"<<mp[nums[i]]<<endl;
             if(mp[nums[i]]>=2){
                 flag++;
                 ind=i;
-                cout<<mp[i]<<" "<<ind<<" "<<flag;
                 break;
             }
         }
-        cout<<ind<<" "<<flag;
         if(flag==0)   return 0;
         return ((ind/3)+1);
     }
