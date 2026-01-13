c1ecc17ddb7b741513f4b7eccb0d158f00b9b7d8
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-01-13 16:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (41.60%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,15 +11,35 @@
  */
 class Solution {
 public:
-    vector<int> v;
-    void helper(TreeNode* root){
-        if(root==nullptr) return;
-        v.push_back(root->val);
-        helper(root->left);
-        helper(root->right);
-    }
+    
+    // void helper(TreeNode* root){
+    //     if(root==nullptr) return;
+    //     v.push_back(root->val);
+    //     helper(root->left);
+    //     helper(root->right);
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
-        helper(root);
+        vector<int> v;
+        if(root==nullptr)   return v;
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode* temp=new TreeNode();
+            temp=s.top();
+            v.push_back(temp->val);
+            s.pop();
+            if(temp->right==nullptr && temp->left==nullptr) continue;
+            if(temp->right==nullptr){
+                s.push(temp->left);
+                continue;
+            } 
+            if(temp->left==nullptr){
+                s.push(temp->right);
+                continue;
+            } 
+            s.push(temp->right);
+            s.push(temp->left);
+        }
         return v;
     }
 };
