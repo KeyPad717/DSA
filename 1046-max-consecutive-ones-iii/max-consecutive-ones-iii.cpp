947fb4d069cd3242446cf3044f8ea28b6bb43988
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-01-27 18:56:14 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (59.04%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -2,60 +2,15 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n=nums.size();
-        int i=0,j=0,count=0,maxi=0,f=0;
-        if(k==0){
-            for(int i=1;i<n;i++){
-                if(nums[i]==1){
-                    if(nums[i-1]==nums[i]){
-                        count++;
-                        maxi=max(maxi,count);
-                    }
-                }
-                else{
-                    if(nums[i-1]==1){
-                        count++;
-                        maxi=max(maxi,count);
-                    }
-                    count=0;
-                }
+        int l=0,r=0,z=0,maxi=0,f=0;
+        while(r<n){
+            if(nums[r]==0)  z++;
+            if(z>k){
+                if(nums[l]==0)  z--;
+                l++;
             }
-            if(nums[n-1]==1)    maxi++;
-            return maxi;
-        }
-        while(j<n){
-            if(nums[j]==0){
-                while(count<(k+1) && j<n){
-                    if(nums[j]==0) count++;
-                    if(j==n-1){
-                        if(count>=k)    break;
-                        else{
-                            maxi=max(maxi,j-i+1);
-                            f++;
-                            break;
-                        }
-                    }
-                    if(count==(k+1))    break;
-                    j++;
-                }
-                if(f!=0)    break;
-                if(count>k){
-                    j=j-1;
-                    count=k;
-                } 
-                cout<<i<<" "<<nums[i]<<" "<<j<<" "<<nums[j]<<endl;
-                maxi=max(maxi,j-i+1);
-
-                while(count==k && j<n){
-                    if(nums[i]==0){
-                        i++;
-                        break;
-                    }
-                    i++;
-                }
-                j=i;
-                count=0;
-            }
-            else    j++;
+            maxi=max(maxi,r-l+1);
+            r++;
         }
         return maxi;
     }
