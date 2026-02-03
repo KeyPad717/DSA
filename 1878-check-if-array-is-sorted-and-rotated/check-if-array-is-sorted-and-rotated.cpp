c81614d33103d601c34d6f887c71d4c0850d087e
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-02-03 12:28:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (77.99%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n=nums.size(),f=0,pos=0;
-        for(int i=1;i<n;i++){
-            if(nums[i]>=nums[i-1])   f++;
-            else    pos=i;
+        int n=nums.size(),f=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n])   f++;
+            if(f>1) break;
         }
-        if(n-1==f)  return true;
-        f=0;
-        int i=pos,j=-1;
-        while(i!=j){
-            if(i==pos)  j=i;
-            if(i==n-1){
-                if(nums[i]<=nums[0]){
-                    f++;
-                    i=0;
-                    continue;
-                }
-                else break;
-            }
-            if(nums[i]<=nums[i+1]){
-                f++;
-                i++;
-            }
-            else break;
-        }
-        if(f==n-1)  return true;
-        return false;
+        if(f>1) return false;
+        return true;
     }
 };
