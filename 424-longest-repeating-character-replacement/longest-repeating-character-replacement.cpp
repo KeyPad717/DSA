7cf40956d5140b4c63482cd8e2b9f485b9b5e791
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-02-07 11:43:58 +0530

    Time: 7 ms (44.89%) | Memory: 11.2 MB (7.01%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size(),l=0,r=0,maxf=0,len=0,maxi=0;
+        unordered_map<int,int> mp;
+        mp.reserve(26);
+        while(l<n && r<n){
+            mp[s[r]-'A']++;
+            maxf=max(maxf,mp[s[r]-'A']);
+            int rem=r-l+1-maxf;
+            if(rem<=k){
+                maxi=max(maxi,r-l+1);
+                //cout<<l<<" "<<s[l]<<" "<<r<<" "<<s[r]<<" "<<maxi<<endl;
+            }
+            else{
+                mp[s[l]-'A']--;
+                l++;
+            }
+            r++;
+        }
+        return maxi;
+    }
+};
