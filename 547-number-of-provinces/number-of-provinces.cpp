edea54937313cf03f22b64e795bc2005de4a20da
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-05 11:29:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (63.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int count=0, n=isConnected.size();
-        vector<int> res,vis(n+1,0);
+        int n = isConnected.size(), count = 0;
+        vector<int> vis(n+1,0);
         queue<int> q;
-        for(int i=1;i<=n;i++){
-            if(vis[i]==0){
+
+        for(int i = 1; i <= n; i++){
+            if(!vis[i]){
                 count++;
-                vis[i]=1;
                 q.push(i);
+                vis[i] = 1;
+
                 while(!q.empty()){
-                    int x=q.front();
+                    int x = q.front();
                     q.pop();
-                    for(int j=1;j<=n;j++){
-                        if((x-1)!=(j-1)){
-                            if(isConnected[x-1][j-1]==1){
-                                if(vis[j]==0){
-                                    q.push(j);
-                                    vis[j]=1;
-                                }
-                            }
+
+                    for(int j = 1; j <= n; j++){
+                        if(isConnected[x-1][j-1] == 1 && !vis[j]){
+                            vis[j] = 1;
+                            q.push(j);
                         }
                     }
                 }
