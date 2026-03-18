071573cb355d247bff010b9d4679e1395be6c56e
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-18 23:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    int solver(int n, vector<int>& dp){
-        if(n==0)    return(dp[n]=0);
-        if(n==1)    return(dp[n]=1);
-        if(dp[n]!=-1)   return dp[n];
-        return dp[n]=solver(n-1,dp)+solver(n-2,dp);
-    }
+    // int solver(int n, vector<int>& dp){
+    //     if(n==0)    return(dp[n]=0);
+    //     if(n==1)    return(dp[n]=1);
+    //     if(dp[n]!=-1)   return dp[n];
+    //     return dp[n]=solver(n-1,dp)+solver(n-2,dp);
+    // }
     int fib(int n) {
+        if(n<=0)    return n;
         vector<int> dp(n+1,-1);
-        return solver(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+        // return solver(n,dp);
         // if(n==0)    return 0;
         // if(n==1)    return 1;
         // return(fib(n-1)+fib(n-2));
