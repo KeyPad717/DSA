e04783c9196b01ceadadf93296746d29e1f71411
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-20 22:00:01 +0530

    Time: 219 ms (6.22%) | Memory: 9.8 MB (77.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,30 +2,40 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s1="",s2="";
         for(int i=0;i<s.size();i++){
             if(s[i]!=' ')  {
                 if(s[i]>='A' && s[i]<='Z'){
                     char lower = s[i] + ('a' - 'A'); 
-                    s1 += lower;
-                    s2 += lower;
+                    s[i]=lower;
                 }
                 else if(s[i]>='a' && s[i]<='z'){
-                    s1+=s[i];
-                    s2+=s[i];
+                    s[i]=s[i];
                 }
                 else if(s[i]>='0' && s[i]<='9'){
-                    s1+=s[i];
-                    s2+=s[i];
+                    s[i]=s[i];
                 }
-                // s1+=s[i].lower();
-                // s2+=s[i].lower();
+                else{
+                    s.erase(i,1);
+                    i--;
+                }
+            }
+            else{
+                s.erase(i,1);
+                i--;
             }
         }
-        reverse(s1.begin(),s1.end());
-        cout<<s1<<endl;
-        cout<<s2<<endl;
-        if(s1==s2)  return 1;
-        return 0;
+        cout<<s<<endl;
+        //reverse(s1.begin(),s1.end());
+        //if(s1==s2)  return 1;
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(s[i]==' ')   i++;
+            if(s[j]==' ')   j--;
+            cout<<s[i]<<" "<<s[j]<<endl;
+            if(s[i]!=s[j])  return 0;
+            i++;
+            j--;
+        }
+        return 1;
     }
 };
