b0895b4432a173f088d64f4ce4c42e43ac78c61f
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-20 22:35:05 +0530

    Time: 6 ms (6.97%) | Memory: 9.8 MB (93.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,36 +2,13 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
         int i=0,j=s.size()-1;
         while(i<j){
-            cout<<"a: "<<s[i]<<" "<<s[j]<<endl;
-            if(s[i]>='A' && s[i]<='Z'){
-                char lower = s[i] + ('a' - 'A'); 
-                s[i]=lower;
-            }
-            if(s[j]>='A' && s[j]<='Z'){
-                char lower = s[j] + ('a' - 'A'); 
-                s[j]=lower;
-            }
-            if(((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')) && ((s[j]>='a' && s[j]<='z') || (s[j]>='0' && s[j]<='9'))){
-                if(s[i]!=s[j])  return 0;
-                i++;
-                j--;
-            }
-            else{
-                if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
-                    j--;
-                }
-                else if((s[j]>='a' && s[j]<='z') || (s[j]>='0' && s[j]<='9')){
-                    i++;
-                }
-                else{
-                    i++;
-                    j--;
-                }
-            }
-            cout<<"b: "<<s[i]<<" "<<s[j]<<endl;
+            while(i<j && !isalnum(s[i])) i++;
+            while(i<j && !isalnum(s[j])) j--;
+            if(tolower(s[i])!=tolower(s[j]))  return 0;
+            i++;
+            j--;
         }
         return 1;
     }
