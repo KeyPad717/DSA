0de9cbb8564be016346c1b65fbda3254d243c92c
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-20 22:18:08 +0530

    Time: 754 ms (6.22%) | Memory: 10 MB (53.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,39 +2,36 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=' ')  {
-                if(s[i]>='A' && s[i]<='Z'){
-                    char lower = s[i] + ('a' - 'A'); 
-                    s[i]=lower;
-                }
-                else if(s[i]>='a' && s[i]<='z'){
-                    s[i]=s[i];
-                }
-                else if(s[i]>='0' && s[i]<='9'){
-                    s[i]=s[i];
-                }
-                else{
-                    s.erase(i,1);
-                    i--;
-                }
-            }
-            else{
-                s.erase(i,1);
-                i--;
-            }
-        }
-        cout<<s<<endl;
-        //reverse(s1.begin(),s1.end());
-        //if(s1==s2)  return 1;
+        
         int i=0,j=s.size()-1;
         while(i<j){
-            if(s[i]==' ')   i++;
-            if(s[j]==' ')   j--;
-            cout<<s[i]<<" "<<s[j]<<endl;
-            if(s[i]!=s[j])  return 0;
-            i++;
-            j--;
+            cout<<"a: "<<s[i]<<" "<<s[j]<<endl;
+            if(s[i]>='A' && s[i]<='Z'){
+                char lower = s[i] + ('a' - 'A'); 
+                s[i]=lower;
+            }
+            if(s[j]>='A' && s[j]<='Z'){
+                char lower = s[j] + ('a' - 'A'); 
+                s[j]=lower;
+            }
+            if(((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')) && ((s[j]>='a' && s[j]<='z') || (s[j]>='0' && s[j]<='9'))){
+                if(s[i]!=s[j])  return 0;
+                i++;
+                j--;
+            }
+            else{
+                if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
+                    j--;
+                }
+                else if((s[j]>='a' && s[j]<='z') || (s[j]>='0' && s[j]<='9')){
+                    i++;
+                }
+                else{
+                    i++;
+                    j--;
+                }
+            }
+            cout<<"b: "<<s[i]<<" "<<s[j]<<endl;
         }
         return 1;
     }
