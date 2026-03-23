616fdbde18385c35cc32a629f7a16aa45bb39953
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-23 11:27:13 +0530

    Time: 202 ms (57.23%) | Memory: 146.3 MB (47.09%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    vector<int> temp;
     void merge(vector<int>& nums, int low, int mid, int high){
-        temp.clear();
-        int left=low;
-        int right=mid+1;
+        vector<int> temp;
+        temp.reserve(high-low+1);
+        int left=low, right=mid+1, k=0;
         while(left<=mid && right<=high){
             if(nums[left]<=nums[right]){
-                temp.push_back(nums[left]);
-                left++;
+                temp[k++]=nums[left++];
             }
             else{
-                temp.push_back(nums[right]);
-                right++;
+                temp[k++]=nums[right++];
             }
         }
         while(left<=mid){
-            temp.push_back(nums[left]);
-            left++;
+            temp[k++]=nums[left++];
         }
         while(right<=high){
-            temp.push_back(nums[right]);
-            right++;
+            temp[k++]=nums[right++];
         }
         for(int i=low;i<=high;i++){
             nums[i]=temp[i-low];
