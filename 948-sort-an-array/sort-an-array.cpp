b7e4cbef8607dec7fc2a605a36d697f405f2cef4
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-23 11:33:41 +0530

    Time: 61 ms (62.79%) | Memory: 72.9 MB (62.75%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int low, int mid, int high){
-        vector<int> temp(high-low+1);
-        int left=low, right=mid+1, k=0;
+    void merge(vector<int>& nums, int low, int mid, int high, vector<int>& temp){
+        int left=low, right=mid+1, k=low;
         while(left<=mid && right<=high){
             if(nums[left]<=nums[right]){
                 temp[k++]=nums[left++];
@@ -18,18 +17,20 @@ public:
             temp[k++]=nums[right++];
         }
         for(int i=low;i<=high;i++){
-            nums[i]=temp[i-low];
+            nums[i]=temp[i];
         }
     }
-    void mergeSort(vector<int>& nums, int low, int high){
+    void mergeSort(vector<int>& nums, int low, int high, vector<int>& temp){
         if(low>=high)   return;
         int mid=low+(high-low)/2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid+1, high);
-        merge(nums, low, mid, high);
+        mergeSort(nums, low, mid, temp);
+        mergeSort(nums, mid+1, high, temp);
+        merge(nums, low, mid, high, temp);
     }
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        if(nums.empty())    return nums;
+        vector<int> temp(nums.size());
+        mergeSort(nums, 0, nums.size()-1, temp);
         return nums;
     }
 };
