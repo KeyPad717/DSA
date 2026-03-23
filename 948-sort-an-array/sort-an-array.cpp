c1b0f78703bbbebe38ef7d6733c251af1afed5f6
KeyPad717 <77205289+KeyPad717@users.noreply.github.com>
2026-03-23 11:22:21 +0530

    Time: 63 ms (62.23%) | Memory: 76.7 MB (52.91%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,40 +1,41 @@
-    class Solution {
-    public:
-        vector<int> temp;
-        void merge(vector<int>& nums, int low, int mid, int high){
-            temp.clear();
-            int left=low, right=mid+1;
-            while(left<=mid && right<=high){
-                if(nums[left]<=nums[right]){
-                    temp.push_back(nums[left]);
-                    left++;
-                }
-                else{
-                    temp.push_back(nums[right]);
-                    right++;
-                }
-            }
-            while(left<=mid){
+class Solution {
+public:
+    vector<int> temp;
+    void merge(vector<int>& nums, int low, int mid, int high){
+        temp.clear();
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
                 temp.push_back(nums[left]);
                 left++;
             }
-            while(right<=high){
+            else{
                 temp.push_back(nums[right]);
                 right++;
             }
-            for(int i=low;i<=high;i++){
-                nums[i]=temp[i-low];
-            }
         }
-        void mergeSort(vector<int>& nums, int low, int high){
-            if(low>=high)   return;
-            int mid=low+(high-low)/2;
-            mergeSort(nums,low,mid);
-            mergeSort(nums,mid+1,high);
-            merge(nums,low,mid,high);
+        while(left<=mid){
+            temp.push_back(nums[left]);
+            left++;
         }
-        vector<int> sortArray(vector<int>& nums) {
-            mergeSort(nums,0,nums.size()-1);
-            return nums;
+        while(right<=high){
+            temp.push_back(nums[right]);
+            right++;
         }
-    };
+        for(int i=low;i<=high;i++){
+            nums[i]=temp[i-low];
+        }
+    }
+    void mergeSort(vector<int>& nums, int low, int high){
+        if(low>=high)   return;
+        int mid=low+(high-low)/2;
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid+1, high);
+        merge(nums, low, mid, high);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
