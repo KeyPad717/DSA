a220e61905ef2b26ccdbff1ae8f37ac54c66ea85
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-04-15 13:25:25 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (68.33%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,26 +4,22 @@ public:
         int left=0, right=nums.size()-1;
         while(left<right){
             int mid=left+(right-left)/2;
-            cout<<mid<<" "<<nums[mid]<<" "<<endl;
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])    return nums[mid];
             if(nums[mid-1]==nums[mid]){
-                if((mid+1)%2==0){
-                    left=mid+1;
-                    cout<<"a"<<endl;
+                if((mid+1)%2){
+                    right=mid+1;   
                 }
                 else{
-                    right=mid+1;
-                    cout<<"b"<<endl;
+                    left=mid+1;
                 }
             }
             else{
-                if((mid)%2==0){
-                    left=mid+1;
-                    cout<<"c"<<endl;
+                if((mid)%2){
+                    right=mid-1;
+                    
                 }
                 else{
-                    right=mid-1;
-                    cout<<"d"<<endl;
+                    left=mid+1;
                 }
             }
         }
