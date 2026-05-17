379abe6c4798e4c3871eb771d1fd4c23b8c20901
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-17 16:39:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (12.77%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int left=1, right=x;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(pow(mid,2)==x)   return mid;
+            else if (pow(mid,2)>x)  right=mid-1;
+            else                    left=mid+1;
+        }
+        return right;
     }
 };
