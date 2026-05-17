3ce8edafd53d08c37eb46ab24656901260ffc6bc
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-17 22:18:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (6.82%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty())
-            return false;
-
+        
         int n = matrix.size();
         int m = matrix[0].size();
 
