3bd2a1a518feb9746d0d96676abfb7e6c9f06f05
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-18 17:28:20 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (67.67%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int n=mat.size(), m=mat[0].size(),x=-1,y=-1;
-        if(n==1 && m==1)    return {0,0};
-        int left=0, right=m-1;
-        //vector<vector<int>> v = {{0,1},{0,-1}};        
-        while(1){
-            int mid=left+(right-left)/2, maxi=-1;
-            for(int i=0;i<n;i++){
-                if(mat[i][mid]>maxi){
-                    maxi=mat[i][mid];
-                    x=i;
-                    y=mid;
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int left = 0, right = m - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Find row having maximum element in mid column
+            int maxRow = 0;
+            for (int i = 0; i < n; i++) {
+                if (mat[i][mid] > mat[maxRow][mid]) {
+                    maxRow = i;
                 }
             }
-            if(y>=1 && y<=m-2){
-                if(mat[x][y]>mat[x][y-1] && mat[x][y]>mat[x][y+1]){
-                    return {x,y};
-                }
-                else if (mat[x][y]>mat[x][y-1]) left=mid+1;
-                else                            right=mid-1;
+
+            int leftVal  = (mid - 1 >= 0) ? mat[maxRow][mid - 1] : -1;
+            int rightVal = (mid + 1 < m) ? mat[maxRow][mid + 1] : -1;
+
+            // Peak found
+            if (mat[maxRow][mid] > leftVal &&
+                mat[maxRow][mid] > rightVal) {
+                return {maxRow, mid};
             }
-            else if (y==0){
-                if(mat[x][y]>mat[x][y+1])   return {x,y};
-                else                        left=mid+1;
-            }
-            else{
-                if(mat[x][y]>mat[x][y-1])   return {x,y};
-                else                        right=mid-1;
+
+            // Move toward larger neighbor
+            if (mat[maxRow][mid] < rightVal) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return {x,y};
+
+        return {-1, -1};
     }
 };
