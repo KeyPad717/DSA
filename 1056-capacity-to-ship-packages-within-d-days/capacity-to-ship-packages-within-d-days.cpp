aff7e6015874770e7aa9240e9c2e50bc2447776b
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-20 17:04:59 +0530

    Time: 16 ms (20.75%) | Memory: 35.1 MB (18.50%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int left=*max_element(weights.begin(), weights.end()), right=accumulate(weights.begin(), weights.end(),0), n=weights.size();
-        while(left<=right){
-            int mid=left+(right-left)/2, minWeight=0, day=1;
-            //cout<<"a "<<left<<" "<<mid<<" "<<right<<endl;
-            for(int i=0;i<n;i++){
-                if(minWeight+weights[i]<=mid){
-                    minWeight+=weights[i];
-                }
-                else if(minWeight+weights[i]>mid){
+
+        int left = *max_element(weights.begin(), weights.end());
+        int right = accumulate(weights.begin(), weights.end(), 0);
+
+        int n = weights.size();
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int day = 1;
+            int load = 0;
+
+            for (int i = 0; i < n; i++) {
+
+                if (load + weights[i] > mid) {
                     day++;
-                    minWeight=weights[i];
+                    load = weights[i];
                 }
-               // cout<<weights[i]<<" "<<minWeight<<" "<<day<<" "<<days<<endl;
+                else {
+                    load += weights[i];
+                }
             }
-            //cout<<day<<endl;
-            //cout<<left<<" "<<mid<<" "<<right<<" "<<day<<" "<<days<<endl;
-            if(day<=days)    right=mid-1;
-            else            left=mid+1;
+
+            if (day <= days) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
         }
+
         return left;
     }
 };
