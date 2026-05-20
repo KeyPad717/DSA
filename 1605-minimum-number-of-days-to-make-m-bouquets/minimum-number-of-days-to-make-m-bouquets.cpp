c046d22d6cc8b34381ef027efd35ce2f171b3daa
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-20 15:25:23 +0530

    Time: 29 ms (18.97%) | Memory: 70.6 MB (45.16%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int n=bloomDay.size();
-        long long mul=1LL*m*k;
-        if(mul>n)   return -1;
-        int left=*min_element(bloomDay.begin(),bloomDay.end());
-        int right=*max_element(bloomDay.begin(),bloomDay.end());
-        while(left<=right){
-            int mid=left+(right-left)/2, bou=0, adj=0;
-            for(int i=0;i<n;i++){
-                if(bloomDay[i]<=mid){
-                    adj++;
-                    if(adj==k){
-                        bou++;
-                        adj=0;
-                    }
-                }
-                else{
-                    adj=0;
-                }
+    
+    bool canMake(vector<int>& bloomDay, int m, int k, int day) {
+        int n = bloomDay.size();
+
+        int flowers = 0;
+        int bouquets = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (bloomDay[i] <= day) {
+                flowers++;
+            } 
+            else {
+                flowers = 0;
             }
-            //cout<<left<<" "<<mid<<" "<<right<<" "<<bou<<" "<<m<<endl;
-            if(bou<m){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
+
+            if (flowers == k) {
+                bouquets++;
+                flowers = 0;
             }
         }
+
+        return bouquets >= m;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+
+        long long need = 1LL * m * k;
+
+        if (need > bloomDay.size()) {
+            return -1;
+        }
+
+        int left = *min_element(bloomDay.begin(), bloomDay.end());
+        int right = *max_element(bloomDay.begin(), bloomDay.end());
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (canMake(bloomDay, m, k, mid)) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
         return left;
     }
 };
