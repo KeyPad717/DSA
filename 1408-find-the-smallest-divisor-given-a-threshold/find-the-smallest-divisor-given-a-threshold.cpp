eb9813716733c73932a367cb958dbd0a0e0c1a5c
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-20 15:35:33 +0530

    Time: 5 ms (71.86%) | Memory: 26.1 MB (79.45%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -5,9 +5,11 @@ public:
         while(left<=right){
             int mid=left+(right-left)/2, sum=0;
             for(int i=0;i<n;i++){
-                if(nums[i]%mid==0)      sum+=nums[i]/mid;
-                else if(nums[i]<mid)    sum+=1;
-                else                    sum+=nums[i]/mid+1;
+                // if(nums[i]%mid==0)      sum+=nums[i]/mid;
+                // else if(nums[i]<mid)    sum+=1;
+                // else                    sum+=nums[i]/mid+1;
+                sum += ( nums[i] + mid -1 )/ mid;
+
             }
             if(sum>threshold){
                 left=mid+1;
