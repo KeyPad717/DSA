1e79d04d82c474677da15e5e18839dd0f0f70a3a
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-20 14:58:00 +0530

    Time: 3 ms (94.79%) | Memory: 22.9 MB (44.77%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n=piles.size(), maxi=piles[0], ans=INT_MAX;
-        for(int i=1;i<n;i++){
-            maxi=max(maxi,piles[i]);
-        }
-        int left=1, right=maxi;
+        int n=piles.size();
+        int left=1, right = *max_element(piles.begin(), piles.end());;
         while(left<=right){
             int mid=left+(right-left)/2;
             long long sum=0LL;
@@ -13,14 +10,13 @@ public:
                 if(piles[i]<mid)    sum+=1;
                 else if(piles[i]%mid==0)    sum+=piles[i]/(mid);
                 else                sum+=(piles[i])/(mid)+1;
-                // cout<<left<<" "<<mid<<" "<<right<<" "<<piles[i]<<" "<<mid<<" "<<(piles[i])/(mid)<<" "<<sum<<endl;
+                //alternative better :- sum += (piles[i] + mid - 1) / mid;
             }
             if(sum>h)  left=mid+1;
             else{
-                ans=min(mid,ans);
                 right=mid-1;
             }
         }
-        return ans;
+        return left;
     }
 };
