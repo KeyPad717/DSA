5379f354cd224dd5ac2b4c71c2f24b648e4cf64d
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-20 14:58:45 +0530

    Time: 7 ms (69.65%) | Memory: 22.9 MB (76.94%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -7,11 +7,10 @@ public:
             int mid=left+(right-left)/2;
             long long sum=0LL;
             for(int i=0;i<n;i++){
-                sum += (piles[i] + mid - 1) / mid;
-                // if(piles[i]<mid)    sum+=1;
-                // else if(piles[i]%mid==0)    sum+=piles[i]/(mid);
-                // else                sum+=(piles[i])/(mid)+1;
-                //alternative better :- 
+                if(piles[i]<mid)    sum+=1;
+                else if(piles[i]%mid==0)    sum+=piles[i]/(mid);
+                else                sum+=(piles[i])/(mid)+1;
+                //alternative better :- sum += (piles[i] + mid - 1) / mid;
             }
             if(sum>h)  left=mid+1;
             else{
