6fb371e27ed87f8e5e5f3ae44d2c1bafcca62d73
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-25 21:08:39 +0530

    Time: 3 ms (27.73%) | Memory: 12.8 MB (20.94%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    void helper(int idx, int n, vector<vector<int>>& v1, vector<int>& v, vector<int>& nums){
-        if(idx==n){
-            v1.push_back(v);
-            return ;
+
+    void helper(int index,
+                vector<int>& nums,
+                vector<int>& current,
+                vector<vector<int>>& result) {
+
+        // Base case:
+        // One subset formed.
+        if (index == nums.size()) {
+            result.push_back(current);
+            return;
         }
-        helper(idx+1, n, v1, v, nums);
-        v.push_back(nums[idx]);
-        helper(idx+1, n, v1, v, nums);
-        v.pop_back();
+
+        // Exclude current element
+        helper(index + 1, nums, current, result);
+
+        // Include current element
+        current.push_back(nums[index]);
+        helper(index + 1, nums, current, result);
+        current.pop_back();
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> v1;
-        vector<int> v;
-        int n=nums.size();
-        helper(0, n, v1, v, nums);
-        return v1;
+
+        vector<vector<int>> result;
+        vector<int> current;
+
+        helper(0, nums, current, result);
+
+        return result;
     }
 };
