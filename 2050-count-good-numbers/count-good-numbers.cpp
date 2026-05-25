4e821402df1f651225abe2ae314ef9713de9a1bc
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-25 12:03:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.29%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
+    static const long long MOD = 1e9 + 7;
+
     long long myPow(long long x, long long n) {
-        long long power = n;
+        long long result = 1;
 
-        long long result = 1LL;
-
-        while (power > 0) {
-            // If power is odd
-            if (power % 2 == 1) {
-                result = (result * x) % 1000000007;
+        while (n > 0) {
+            if (n & 1) {
+                result = (result * x) % MOD;
             }
 
-            x = (x * x) % 1000000007;
-            power /= 2;
+            x = (x * x) % MOD;
+            n >>= 1;
         }
 
-        return result % 1000000007;
+        return result;
     }
+
     int countGoodNumbers(long long n) {
-        if(n==1)    return 5;
-        if(n==2)    return 20;
-        return (myPow(5,(n+1)/2) * myPow(4,n/2)) % 1000000007;
+        return (myPow(5, (n + 1) / 2) *
+                myPow(4, n / 2)) % MOD;
     }
 };
