012a9096e99ebb3fb2610e3aea3bbb89bb01bfd7
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-25 10:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.81%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,53 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+    double ans=1, sign=1, f=0;
+    double myPow(double x, int n1) {
+        long long n=n1;
+        if(n==0)    return 1;
+        if(x==1)    return 1;
+        if(n<0) {
+            sign=-1;
+            n=0-n;
+        }
+        if(n==1){
+            if(sign==-1)    return 1/x;
+            return x;
+        }
+        if(n%2){
+            n--;
+            ans*=x;
+            while(n>1){
+                if(n%2){
+                    n--;
+                    ans*=x;
+                    x*=x;
+                    n/=2;
+                }
+                else{
+                    x*=x;
+                    n/=2;
+                }
+            }
+            ans*=x;
+            if(sign==-1)    return 1/ans;
+            return ans;
+        }
+        
+        while(n>1){
+            cout<<n<<" "<<x<<" "<<ans<<endl;
+            if(n%2){
+                n--;
+                ans*=x;
+                x*=x;
+                n/=2;
+            }
+            else{
+                x*=x;
+                n/=2;
+            }
+        }
+        ans*=x;
+        if(sign==-1)    return 1/ans;
+        return ans;
     }
 };
