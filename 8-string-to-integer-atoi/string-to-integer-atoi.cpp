8b737c6284a329db0149843c23560d190ea611c1
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-25 09:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (80.64%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,93 +1,31 @@
 class Solution {
 public:
-    string s="";
-    long long int i=0,sign=1,number=0;
-    void whitespace(string s){
-        if(s[i]==' '){
-            whitespace(s.substr(1,s.length()-1));
-        }
-        sign_num(s);
-    }
-    void sign_num(string s){
-        if(s[i]=='-'){
-            sign=0;
-            num(s.substr(1,s.length()-1));
-        }
-        else if(s[i]=='+'){
-            num(s.substr(1,s.length()-1));
-        }
-        num(s);
-    }
-    void num(string s){
-        //cout<<number<<" ";
-        if(s[i]=='0'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+0;
-            num(s.substr(1,s.length()-1));
-        }  
-        else if(s[i]=='1'){
-            //cout<<"l"<<" ";
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+1;
-            num(s.substr(1,s.length()-1));
-        } 
-        else if(s[i]=='2'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+2;
-            num(s.substr(1,s.length()-1));
-        } 
-        else if(s[i]=='3'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+3;
-            num(s.substr(1,s.length()-1));
-        } 
-        else if(s[i]=='4'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+4;
-            num(s.substr(1,s.length()-1));
-        } 
-        else if(s[i]=='5'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+5;
-            num(s.substr(1,s.length()-1));
-        } 
-        else if(s[i]=='6'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+6;
-            num(s.substr(1,s.length()-1));
-        }  
-        else if(s[i]=='7'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+7;
-            num(s.substr(1,s.length()-1));
-        } 
-        else if(s[i]=='8'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+8;
-            num(s.substr(1,s.length()-1));
-        } 
-        else if(s[i]=='9'){
-            if(number<-2147483648)   return;
-            if(number>2147483647)   return;
-            number=number*10+9;
-            num(s.substr(1,s.length()-1));
-        }
-        return;
-    }
     int myAtoi(string s) {
-        whitespace(s);
-        if(sign==0)     number=0-number;
-        if(number<-2147483648)   return(-2147483648);
-        if(number>2147483647)   return(2147483647);
-        return(number);
-    }   
+        int power=10, n=s.size(), f=0, sign=1, f1=0;
+        long long ans=0LL;
+        for(int i=0;i<n;i++){
+            if(f1==0 && s[i]==' ')   continue;
+            if(f==0 && s[i]=='-'){
+                f++;
+                f1++;
+                sign=-1;
+                continue;
+            }
+            if(f==0 && s[i]=='+'){
+                f++;
+                f1++;
+                continue;
+            }
+            f1++;
+            f++;
+            int r=s[i]-'0';
+            if(r>=0 && r<=9){
+                ans=ans*power+r;
+                if(ans*sign>=2147483647) return sign*2147483647;
+                if(ans*sign<=-2147483648) return sign*2147483648;
+            }
+            else    break;
+        }
+        return ans*sign;
+    }
 };
