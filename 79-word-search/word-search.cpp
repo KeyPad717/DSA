8f1c41777b8985998e4d0c51c670b05cdac71b3e
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-27 16:07:45 +0530

    Time: 1088 ms (7.04%) | Memory: 287.3 MB (5.05%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    void check(vector<vector<char>>& board, string word, int idx, int sz, string temp, int n, int m, int sz1, int sz2, bool& ans, vector<vector<int>> bound, vector<vector<int>>& vis){
-        if(idx==sz && temp==word){
+    void check(vector<vector<char>>& board, string word, int idx, int sz, int n, int m, int sz1, int sz2, bool& ans, vector<vector<int>> bound, vector<vector<int>>& vis){
+        if(idx==sz){
             ans=true;
             return ;
         }
         for(int i=0;i<4;i++){
             int row=n+bound[i][0];
             int col=m+bound[i][1];
-            //cout<<"a: "<<sz<<" "<<idx<<" "<<row<<" "<<col<<" "<<vis[row][col]<<" "<<board[row][col]<<" "<<temp<<" "<<endl;
             if(row>=0 && row<sz1 && col>=0 && col<sz2 && vis[row][col]==-1){
                 if(board[row][col]==word[idx]){
-                    string x=temp;
-                    temp+=board[row][col];
                     vis[row][col]=1;
-                    //cout<<"b: "<<idx<<" "<<row<<" "<<col<<" "<<board[row][col]<<" "<<temp<<" "<<endl;
-                    check(board, word, idx+1, sz, temp, row, col, sz1, sz2, ans, bound, vis);
+                    check(board, word, idx+1, sz, row, col, sz1, sz2, ans, bound, vis);
                     if(ans==true)   return ;
                     vis[row][col]=-1;
-                    temp=x;
                 }
             }
         }
@@ -28,7 +23,6 @@ public:
         unordered_map<char,int> wordFreq;
         int n=board.size(), m=board[0].size(), sz=word.size();
         bool ans=false;
-        string temp="";
         vector<vector<int>> bound{{0,1}, {0,-1}, {1,0}, {-1,0}};
         vector<vector<int>> vis(n,vector<int>(m,-1));
         for(int i=0;i<n;i++)
@@ -42,13 +36,10 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word[0]){
-                    temp+=board[i][j];
-                    //cout<<i<<" "<<j<<" "<<board[i][j]<<" "<<temp<<" "<<endl;
                     vis[i][j]=1;
-                    check(board, word, 1, sz, temp, i, j, n, m, ans, bound, vis);
+                    check(board, word, 1, sz, i, j, n, m, ans, bound, vis);
                     if(ans) return true;
                     vis[i][j]=-1;
-                    temp="";
                 }
             }
         }
