2ff8bfd9bb54a68c0e8c4e9d7c2028da29421d85
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-27 16:18:18 +0530

    Time: 52 ms (96.74%) | Memory: 11.8 MB (18.62%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    void check(vector<vector<char>>& board, string word, int idx, int sz, int n, int m, int sz1, int sz2, bool& ans, vector<vector<int>> bound){
-        if(idx==sz){
+    const vector<vector<int>> bound{{0,1}, {0,-1}, {1,0}, {-1,0}};
+    void check(vector<vector<char>>& board, string word, int idx, int sz, int n, int m, int sz1, int sz2, bool& ans){
+        if(ans==true || idx==sz){
             ans=true;
             return ;
         }
@@ -12,7 +13,7 @@ public:
                 if(board[row][col]==word[idx]){
                     char temp=board[row][col];
                     board[row][col]='#';
-                    check(board, word, idx+1, sz, row, col, sz1, sz2, ans, bound);
+                    check(board, word, idx+1, sz, row, col, sz1, sz2, ans);
                     if(ans==true)   return ;
                     board[row][col]=temp;
                 }
@@ -24,7 +25,6 @@ public:
         unordered_map<char,int> wordFreq;
         int n=board.size(), m=board[0].size(), sz=word.size();
         bool ans=false;
-        vector<vector<int>> bound{{0,1}, {0,-1}, {1,0}, {-1,0}};
         for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
                 boardFreq[board[i][j]]++;
@@ -38,7 +38,7 @@ public:
                 if(board[i][j]==word[0]){
                     char temp=board[i][j];
                     board[i][j]='#';
-                    check(board, word, 1, sz, i, j, n, m, ans, bound);
+                    check(board, word, 1, sz, i, j, n, m, ans);
                     if(ans) return true;
                     board[i][j]=temp;
                 }
