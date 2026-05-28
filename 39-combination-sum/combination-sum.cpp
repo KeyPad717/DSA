45557bc500613a64dd66dfb58ca000c06ddacb55
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-28 23:11:52 +0530

    Time: 1 ms (61.75%) | Memory: 14.1 MB (73.27%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     void helper(vector<int>& candidates, int target, int idx, int n, vector<vector<int>>& v1, vector<int>& v){
         if(target==0){
-            
             v1.push_back(v);
             return ;
         }
-        if(idx==n){
+        if(idx>=n){
             return ;
         }
         if(candidates[idx]<=target) {
@@ -14,9 +13,7 @@ public:
             helper(candidates, target-candidates[idx], idx, n, v1, v);
             v.pop_back();
         }
-        //v.push_back(candidates[idx]);
         helper(candidates, target, idx+1, n, v1, v);
-        //v.pop_back();
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int n=candidates.size();
