cb35a2da1a156abfbc09fd2b53aff26f366a20aa
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-30 11:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (26.79%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,24 +11,29 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==nullptr || head->next==nullptr || head->next->next==nullptr) return head;
-        ListNode* temp1=head;
-        ListNode* temp2=head->next;
-        ListNode* oddHead=temp1;
-        ListNode* evenHead=temp2;
-        while(temp1!=nullptr && temp2!=nullptr && temp2->next!=nullptr && temp1->next!=nullptr){
+        if(!head || !head->next)    return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* temp1=odd;
+        ListNode* temp2=even;
+        while(temp1 && temp1->next){
+            //cout<<temp1->val<<" "<<temp2->val<<endl;
+            if(temp1->next->next==nullptr){
+                break;
+            }
             temp1->next=temp1->next->next;
-            temp2->next=temp2->next->next;
             temp1=temp1->next;
-            temp2=temp2->next;
+            if(temp2->next) {
+                temp2->next=temp2->next->next;
+                temp2=temp2->next;
+            }
+            else    {
+                temp2->next=nullptr;
+                break;
+            }
+            //cout<<temp1->val<<" "<<temp2->val<<endl;
         }
-        if(temp1->next==nullptr){
-            temp2=nullptr; 
-            temp1->next=evenHead;
-        }
-        else{
-            temp1->next=evenHead;
-        }
+        temp1->next=even;
         return head;
     }
 };
