cc5a39d02c6f43db062fca37222d5a2bad5fc447
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-05-30 10:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 117.8 MB (93.28%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,17 +11,16 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode* head2= new ListNode(head->val);
-        ListNode* temp=head;
-        ListNode* temp1=head2;
-        while(temp && temp->next){
-            temp=temp->next;
-            ListNode* x=new ListNode(temp->val);
-            temp1->next=x;
-            temp1=temp1->next;
+        ListNode* slow=head;
+        ListNode* slow1;
+        ListNode* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        temp=head;
-        ListNode* curr=head2;
+        if(!fast) slow1=slow;
+        else            slow1=slow->next;
+        ListNode* curr=slow1;
         ListNode* prev=nullptr;
         while(curr){
             ListNode* nex=curr->next;
@@ -29,12 +28,11 @@ public:
             prev=curr;
             curr=nex;
         }
-        temp1=prev;
-        while(temp && temp->next){
-            cout<<temp->val<<" "<<temp1->val<<endl;
-            if(temp->val!=temp1->val)   return false;
-            temp=temp->next;
-            temp1=temp1->next;
+        fast=head;
+        while(prev && fast!=slow){
+            if(prev->val!=fast->val)    return false;
+            prev=prev->next;
+            fast=fast->next;
         }
         return true;
     }
