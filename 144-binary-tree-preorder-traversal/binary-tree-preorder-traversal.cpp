ea88d837ef63b2fe5396db401d408eb215950c70
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-01 17:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (72.31%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,18 +11,15 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int>& res){
-        if(!root) {
-            return ;
-        }
-        //cout<<root->val<<endl;
+    void preorder(TreeNode* root, vector<int>& res){
+        if(!root)   return ;
         res.push_back(root->val);
-        helper(root->left, res);
-        helper(root->right, res);
+        preorder(root->left, res);
+        preorder(root->right, res);
     }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
-        helper(root, res);
+        preorder(root, res);
         return res;
     }
 };
