708149d9cd24e6067012e30daf5d7f7ab50f33db
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-02 15:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (73.92%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,61 +11,29 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp1=l1;
-        ListNode* temp2=l2;
-        int carry=0;
-        ListNode* dummy=new ListNode(-1);
-        ListNode* head=dummy;
-        while(temp1 && temp2){
-            int x=carry+temp1->val+temp2->val;
-            ListNode* newNode=new ListNode();
-            if(x>9) {
-                newNode->val=x%10;
-                carry=1;
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry;
+
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
             }
-            else{
-                newNode->val=x;
-                carry=0;
+
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
             }
-            dummy->next=newNode;
-            dummy=dummy->next;
-            temp1=temp1->next;
-            temp2=temp2->next;
+
+            carry = sum / 10;
+
+            tail->next = new ListNode(sum % 10);
+            tail = tail->next;
         }
-        while(temp1){
-            int x=carry+temp1->val;
-            ListNode* newNode=new ListNode();
-            if(x>9) {
-                newNode->val=x%10;
-                carry=1;
-            }
-            else{
-                newNode->val=x;
-                carry=0;
-            }
-            dummy->next=newNode;
-            dummy=dummy->next;
-            temp1=temp1->next;
-        }
-        while(temp2){
-            int x=carry+temp2->val;
-            ListNode* newNode=new ListNode();
-            if(x>9) {
-                newNode->val=x%10;
-                carry=1;
-            }
-            else{
-                newNode->val=x;
-                carry=0;
-            }
-            dummy->next=newNode;
-            dummy=dummy->next;
-            temp2=temp2->next;
-        }
-        if(carry==1){
-            ListNode* newNode=new ListNode(1);
-            dummy->next=newNode;
-        }
-        return head->next;
+
+        return dummy->next;
     }
 };
