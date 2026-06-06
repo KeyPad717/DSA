5ca4102b3e8963d56d2afb64bca9142fc298c82d
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-06 22:04:59 +0530

    Time: 515 ms (5.00%) | Memory: 23.4 MB (98.14%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        for(int i=1;i<intervals.size();i++){
+            cout<<intervals[i-1][0]<<" "<<intervals[i-1][1]<<endl;
+            cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
+            if(intervals[i][0]==intervals[i-1][1]){
+                intervals[i][0]=intervals[i-1][0];
+                intervals.erase(intervals.begin()+i-1);
+                i--;
+            }
+            else if(intervals[i][0]<intervals[i-1][1]){
+                intervals[i][0]=intervals[i-1][0];
+                intervals[i][1]=max(intervals[i-1][1],intervals[i][1]);
+                intervals.erase(intervals.begin()+i-1);
+                i--;
+            }
+        }
+        return intervals;
+    }
+};
