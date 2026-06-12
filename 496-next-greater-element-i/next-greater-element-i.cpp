ca4e232a38d66655856a9f8f8be2e593de156ed6
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-12 15:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (75.38%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,11 +3,11 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
         int n1=nums1.size(), n2=nums2.size();
-        vector<int> v(n2);
+        unordered_map<int,int> mp;
         vector<int> res(n1);
         for(int i=n2-1;i>=0;i--){
             if(st.empty()){
-                v[i]=-1;
+                mp[nums2[i]]=-1;
                 st.push(nums2[i]);
             } 
             else{
@@ -17,19 +17,17 @@ public:
                     else break;
                 }
                 if(st.empty()){
-                    v[i]=-1;
+                    mp[nums2[i]]=-1;
                     st.push(nums2[i]);
                 } 
                 else{
-                    v[i]=st.top();
+                    mp[nums2[i]]=st.top();
                     st.push(nums2[i]);
                 }
             }
         }
         for(int i=0;i<n1;i++){
-            auto x=find(nums2.begin(), nums2.end(), nums1[i]);
-            int idx=distance(nums2.begin(), x);
-            res[i]=v[idx];
+            res[i]=mp[nums1[i]];
         }
         return res;
     }
