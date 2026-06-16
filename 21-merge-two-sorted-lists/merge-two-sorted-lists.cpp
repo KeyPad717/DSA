0cbba6120a83d6907f87a41040f5fba1fff64fbc
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-16 18:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (61.85%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,58 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+ //optimal but not interview ready because of many pointer reassignments and breakings
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1 && !list2)   return list1;
-        if(!list1)  return list2;
-        if(!list2)  return list1;
         ListNode* dummy = new ListNode();
-        ListNode* temp1=list1;
-        ListNode* temp2=list2;
-        ListNode* temp3=dummy;
-        while(temp1 && temp2){
-            if(temp1->val == temp2->val){
-                ListNode* x=temp1;
-                ListNode* y=temp2;
-                temp1=temp1->next;
-                temp2=temp2->next;
-                temp3->next=x;
-                x->next=y;
-                y->next=nullptr;
-                temp3=y;
-            }
-            else if(temp1->val < temp2->val){
-                temp3->next=temp1;
-                ListNode* x=temp1;
-                temp1=temp1->next;
-                x->next=nullptr;
-                temp3=x;
+        ListNode* tail = dummy;
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                tail->next=list1;
+                list1=list1->next;
             }
             else{
-                temp3->next=temp2;
-                ListNode* x=temp2;
-                temp2=temp2->next;
-                x->next=nullptr;
-                temp3=x;
+                tail->next=list2;
+                list2=list2->next;
             }
+            tail=tail->next;
         }
-        while(temp1){
-            temp3->next=temp1;
-            temp3=temp3->next;
-            temp1=temp1->next;
+        if(list1){
+            tail->next=list1;
         }
-        while(temp2){
-            temp3->next=temp2;
-            temp3=temp3->next;
-            temp2=temp2->next;
+        else{
+            tail->next=list2;
         }
         return dummy->next;
     }
