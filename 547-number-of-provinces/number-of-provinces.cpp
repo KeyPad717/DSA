96d34f23d3b84639d50a402bb58c36f3d1830e2f
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-17 21:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (72.88%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    void helper(int node, const vector<vector<int>>& adj, vector<int>& vis){
-        vis[node]=1;
-        for(int x:adj[node]){
-            if(vis[x]==0){
-                vis[x]=1;
-                helper(x, adj, vis);
+    void helper(int row, const vector<vector<int>>& isConnected, vector<int>& vis){
+        vis[row]=1;
+        for(int x=0;x<isConnected.size();x++){
+            if(row!=x && isConnected[row][x]==1){
+                if(vis[x]==0){
+                    helper(x,isConnected,vis);
+                }
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int count=0;
         int V=isConnected.size();
-        vector<vector<int>> adj(V);
         vector<int> vis(V,0);
-        for(int i=0;i<isConnected.size();i++){
-            for(int j=0;j<isConnected[i].size();j++){
-                if(i!=j && isConnected[i][j]==1)    adj[i].push_back(j);
-            }
-        }
         for(int i=0;i<V;i++){
-            if(vis[i]==0){
-                helper(i, adj, vis);
-                count++;
+            for(int j=0;j<V;j++){
+                if(isConnected[i][j]==1){
+                    if(vis[i]==0){
+                        count++;
+                        helper(i,isConnected,vis);
+                    }
+                }
             }
         }
         return count;
