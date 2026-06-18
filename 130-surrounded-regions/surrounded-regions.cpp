d22460a3b5f03e979b0f3f2a437a759a3f69a893
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-18 14:43:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (89.92%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -3,12 +3,12 @@ public:
     const int dr[4]={0,0,-1,1};
     const int dc[4]={1,-1,-0,0};
     void dfs1(int c1, int c2, int m, int n, vector<vector<char>>& board){
-        //board[c1][c2]='P';
+        board[c1][c2]='P';
         for(int i=0;i<4;i++){
             int x=c1+dr[i];
             int y=c2+dc[i];
             if(x>0 && x<m-1 && y>0 && y<n-1 && board[x][y]=='O'){
-                board[x][y]='P';
+                
                 dfs1(x,y,m,n,board);
             }
         }
@@ -24,14 +24,14 @@ public:
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i>0 && i<m-1 && j>0 && j<n-1){
+                //if(i>0 && i<m-1 && j>0 && j<n-1){
                     if(board[i][j]=='P'){
                         board[i][j]='O';
                     }
                     else if(board[i][j]=='O'){
                         board[i][j]='X';
                     }
-                }
+                //}
             } 
         }
     }
