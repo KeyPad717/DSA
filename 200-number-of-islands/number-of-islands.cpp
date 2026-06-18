f4592da2c1d01d1b54b541980b47614fbb51091b
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-18 11:11:50 +0530

    Time: 26 ms (62.94%) | Memory: 17.6 MB (43.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     const vector<vector<int>> bound={{1,0}, {-1,0}, {0,1}, {0,-1}};
-    void dfs(int m, int n, int x, int y, vector<vector<int>>& vis, const vector<vector<char>>& grid){
+    void dfs(int m, int n, int x, int y, vector<vector<char>>& grid){
         for(int i=0;i<4;i++){
             int c1=x+bound[i][0];
             int c2=y+bound[i][1];
-            if(c1>=0 && c1<m && c2>=0 && c2<n && grid[c1][c2]=='1' && vis[c1][c2]==0){
-                vis[c1][c2]=1;
-                dfs(m,n,c1,c2,vis,grid);
+            if(c1>=0 && c1<m && c2>=0 && c2<n && grid[c1][c2]=='1'){
+                grid[c1][c2]='0';
+                dfs(m,n,c1,c2,grid);
             }
         }
     }
@@ -16,9 +16,9 @@ public:
         vector<vector<int>> vis(m, vector<int> (n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && vis[i][j]==0){
-                    vis[i][j]=1;
-                    dfs(m,n,i,j,vis,grid);
+                if(grid[i][j]=='1'){
+                    grid[i][j]='0';
+                    dfs(m,n,i,j,grid);
                     count++;
                 }
             }
