549e4f59177501758a0d7936a2719a163e05c0b2
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-18 11:14:55 +0530

    Time: 25 ms (72.05%) | Memory: 16.3 MB (78.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    const vector<vector<int>> bound={{1,0}, {-1,0}, {0,1}, {0,-1}};
+    const int dr[4]={-1,1,0,0};
+    const int dc[4]={0,0,-1,1};
     void dfs(int m, int n, int x, int y, vector<vector<char>>& grid){
         for(int i=0;i<4;i++){
-            int c1=x+bound[i][0];
-            int c2=y+bound[i][1];
+            int c1=x+dr[i];
+            int c2=y+dc[i];
             if(c1>=0 && c1<m && c2>=0 && c2<n && grid[c1][c2]=='1'){
                 grid[c1][c2]='0';
                 dfs(m,n,c1,c2,grid);
@@ -13,7 +14,6 @@ public:
     }
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size(), n=grid[0].size(), count=0;
-        vector<vector<int>> vis(m, vector<int> (n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
