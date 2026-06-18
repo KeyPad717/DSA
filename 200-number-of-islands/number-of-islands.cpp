2dd7349e73a077a0f662dba7f43777fa7b148025
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-18 11:08:01 +0530

    Time: 29 ms (41.75%) | Memory: 17.6 MB (42.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
+    const vector<vector<int>> bound={{1,0}, {-1,0}, {0,1}, {0,-1}};
+    void dfs(int m, int n, int x, int y, vector<vector<int>>& vis, const vector<vector<char>>& grid){
+        for(int i=0;i<4;i++){
+            int c1=x+bound[i][0];
+            int c2=y+bound[i][1];
+            if(c1>=0 && c1<m && c2>=0 && c2<n && grid[c1][c2]=='1' && vis[c1][c2]==0){
+                vis[c1][c2]=1;
+                dfs(m,n,c1,c2,vis,grid);
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size(), count=0;
-        int n=grid[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> vis(m, vector<int>(n, -1));
+        int m=grid.size(), n=grid[0].size(), count=0;
+        vector<vector<int>> vis(m, vector<int> (n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && vis[i][j]==-1){
-                    q.push({i,j});
-                    int adj[4][2]={{0,-1},{0,1},{1,0},{-1,0}};
-                    while(!q.empty()){
-                        int x=q.front().first;
-                        int y=q.front().second;
-                        q.pop();
-                        vis[x][y]=1;
-                        for(int k=0;k<4;k++){
-                            int a=x+adj[k][0];
-                            int b=y+adj[k][1];
-                            if(a>=0 && b>=0 && a<m && b<n && grid[a][b]=='1' && vis[a][b]==-1){
-                                q.push({a,b});
-                                vis[a][b]=1;
-                            }
-                        }
-                    }
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    vis[i][j]=1;
+                    dfs(m,n,i,j,vis,grid);
                     count++;
                 }
             }
-            
         }
         return count;
     }
