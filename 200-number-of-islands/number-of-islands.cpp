7a5cf2416fb6910734fc95fff9e221b9abc28431
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-22 17:15:16 +0530

    Time: 25 ms (72.25%) | Memory: 16.1 MB (96.54%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -17,7 +17,6 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    grid[i][j]='0';
                     dfs(m,n,i,j,grid);
                     count++;
                 }
