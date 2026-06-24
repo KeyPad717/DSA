ad0c7907f6e318c6d4e058fdf59af532f8e770af
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-24 13:09:08 +0530

    Time: 4 ms (55.06%) | Memory: 19 MB (19.81%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0), res;
+        queue<int> q;
+        for(auto x:prerequisites){
+            adj[x[1]].push_back(x[0]);
+            indegree[x[0]]++;
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)  q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            res.push_back(node);
+            q.pop();
+            for(int x:adj[node]){
+                indegree[x]--;
+                if(indegree[x]==0)  q.push(x);
+            }
+        }
+        if(res.size()!=numCourses){
+            res.clear();
+            return res;
+        }
+        return res;
+    }
+};
