5a24b41a4df50457f8b31d608fce2fe57b32efe2
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-06-26 19:46:23 +0530

    Time: 204 ms (41.76%) | Memory: 350.8 MB (39.88%) - LeetSync

--- 4092-finish-time-of-tasks-i/finish-time-of-tasks-i.cpp
+++ 4092-finish-time-of-tasks-i/finish-time-of-tasks-i.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    long long dfs(int node, long long& finish, long long latest, long long earliest, const vector<vector<int>>& adj, const vector<int>& baseTime, vector<int>& vis){
-        vis[node]=1;
+    long long dfs(int node, const vector<vector<int>>& adj, const vector<int>& baseTime){
         if(adj[node].empty()){
             return baseTime[node];
         }
+        long long latest=LLONG_MIN;
+        long long earliest=LLONG_MAX;
         for(int x:adj[node]){
-            if(vis[x]==0){
-                long long child=dfs(x, finish, LLONG_MIN, LLONG_MAX, adj, baseTime, vis);
-                latest=max(latest, child);
-                earliest=min(earliest, child);
-            }
+            long long child=dfs(x, adj, baseTime);
+            latest=max(latest, child);
+            earliest=min(earliest, child);
         }
-        finish=2*latest-earliest+baseTime[node];
-        return finish;
+        return 2*latest-earliest+baseTime[node];
     }
     long long finishTime(int n, vector<vector<int>>& edges, vector<int>& baseTime) {
         vector<vector<int>> adj(n);
-        vector<int> vis(n,0);
         for(auto x:edges){
             adj[x[0]].push_back(x[1]);
         }
-        long long finish=0;
-        return dfs(0, finish, LLONG_MIN, LLONG_MAX, adj, baseTime, vis);
+        return dfs(0, adj, baseTime);
     }
 };
