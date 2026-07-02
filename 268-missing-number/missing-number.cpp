90037080d781d7916c2a4a78c5b27a4e526a36bd
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-02 13:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (12.58%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums){
-        //better approach :- hashing
-        //this is the optimal one 
-
-        int sum=0,exp=0,n=nums.size();
-        exp = (n*(n+1))/2;
-        for(int i =0;i<nums.size();i++){
-            sum+=nums[i];
-        }
-        return(exp-sum);
-
-        //but it can overflow for large int value, so 
-        //XOR method is more optimal than this approach
-        
+    int missingNumber(vector<int>& nums) {
+        int sum=(nums.size()*(nums.size()+1))/2;
+        int sum1=accumulate(nums.begin(), nums.end(), 0);
+        return abs(sum-sum1);
     }
 };
