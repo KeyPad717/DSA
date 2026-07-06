c0a542fc4ba71273851338c144c5263c338e74f9
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-06 22:54:10 +0530

    Time: 247 ms (5.30%) | Memory: 20.9 MB (50.96%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,29 +1,71 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size(), n=matrix[0].size();
-        vector<int> rows(m,0), cols(n,0);
+        int m=matrix.size(), n=matrix[0].size(), col=matrix[0][0];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    rows[i]=1;
-                    cols[j]=1;
+                    cout<<i<<" "<<j<<" "<<matrix[i][j]<<endl;
+                    if(i==0 && j==0){
+                        col=0;
+                        matrix[i][j]=0;
+                    }
+                    else if(j==0){
+                        col=0;
+                    }
+                    else if(i==0){
+                        matrix[0][0]=0;
+                    }
+                    else{
+                        matrix[0][j]=0;
+                        matrix[i][0]=0;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            if(rows[i]==1){
-                for(int j=0;j<n;j++){
+        for(int i=m-1;i>0;i--){
+            for(int j=n-1;j>0;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            if(cols[i]==1){
-                for(int j=0;j<m;j++){
-                    matrix[j][i]=0;
-                }
+        if(matrix[0][0]==0){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
             }
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
