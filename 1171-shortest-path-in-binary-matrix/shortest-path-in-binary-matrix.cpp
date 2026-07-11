dd1e2b2deb82104904cbd381b8b2c9a6f897cc7d
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-11 18:02:24 +0530

    Time: 31 ms (28.29%) | Memory: 26.2 MB (44.53%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,31 +1,52 @@
 class Solution {
 public:
-    const int dr[8]={0,0,1,-1,1,-1,1,-1};
-    const int dc[8]={1,-1,0,0,1,-1,-1,1};
+    const int dr[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+    const int dc[8] = {1, -1, 0, 0, 1, -1, -1, 1};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        if(grid[0][0]==1)   return -1;
-        queue<pair<int,int>> q;
-        vector<vector<int>> dist(n, vector<int> (n,0));
-        q.push({0,0});
-        grid[0][0]=1;
-        while(!q.empty()){
-            int x=q.front().first;
-            int y=q.front().second;
-            q.pop();
-            if(x==n-1 && y==n-1){
-                return dist[x][y]+1;
+        int n = grid.size();
+        if(grid[0][0]==1 || grid[n-1][n-1]==1)   return -1;
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> vis(n, vector<int>(n, 1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0)
+                    vis[i][j] = 0;
             }
-            for(int i=0;i<8;i++){
-                int c1=dr[i]+x;
-                int c2=dc[i]+y;
-                if(c1>=0 && c1<n && c2>=0 && c2<n && grid[c1][c2]==0){
-                    grid[c1][c2]=1;
-                    dist[c1][c2]=dist[x][y]+1;
-                    q.push({c1,c2});
+        }
+        q.push({{0, 0}, 0});
+        vis[0][0]=1;
+        while (!q.empty()) {
+            int c1 = q.front().first.first;
+            int c2 = q.front().first.second;
+            int dist = q.front().second;
+            q.pop();
+            if (c1 == n - 1 && c2 == n - 1) {
+                // for (int i = 0; i < n; i++) {
+                //     for (int j = 0; j < n; j++) {
+                //         cout<<vis[i][j]<<" ";
+                //     }
+                //     cout<<endl;
+                // }
+                return dist + 1;
+            }
+            for (int i = 0; i < 8; i++) {
+                int x = c1 + dr[i];
+                int y = c2 + dc[i];
+                
+                if (x >= 0 && y >= 0 && x < n && y < n && grid[x][y] == 0 &&
+                    vis[x][y] == 0) {
+                        //cout<<x<<" "<<y<<endl;
+                    q.push({{x, y}, dist + 1});
+                    vis[x][y]=1;
                 }
             }
         }
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         cout<<vis[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         return -1;
     }
 };
