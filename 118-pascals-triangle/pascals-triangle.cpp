54491da387d0a2bc156f66559e3de83ca5e8c421
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-12 19:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (63.62%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -13,10 +13,15 @@ public:
     }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
-        for(int i=1;i<=numRows;i++){
+        res.push_back({1});
+        for(int i=2;i<=numRows;i++){
             vector<int> temp;
-            for(int j=1;j<=i;j++){
-                temp.push_back(value(i,j));
+            long long ans=1;
+            temp.push_back(ans);
+            for(int j=1;j<i;j++){
+                ans*=(i-j);
+                ans/=j;
+                temp.push_back(ans);
             }
             res.push_back(temp);
         }
