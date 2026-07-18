ca87b74f8c2865ae917a9614bdb5494dfb416d6f
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-18 14:54:26 +0530

    Time: 4 ms (11.42%) | Memory: 17.8 MB (23.75%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -4,8 +4,8 @@ public:
         int n=nums.size(), prod=1, maxi=INT_MIN, f=0;
         for(int i=0;i<n;i++){
             if(nums[i]==0){
+                maxi=max(maxi,0);
                 prod=1;
-                f++;
             }  
             else{
                 prod*=nums[i];
@@ -15,16 +15,13 @@ public:
         prod=1;
         for(int i=n-1;i>=0;i--){
             if(nums[i]==0){
-                f++;
+                maxi=max(maxi,0);
                 prod=1;
             }  
             else{
                 prod*=nums[i];
                 maxi=max(maxi,prod);
             }
-        }
-        if(f){
-            return max(0,maxi);
         }
         return maxi;
     }
