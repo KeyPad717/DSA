5858417b5e2083f7bab8b7e535dcffa9e6f6222d
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-20 00:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (22.66%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<int> res;
+        unordered_map<int,int> mp;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
+        for(int x:nums){
+            mp[x]++;
+        }
+        for(auto x:mp){
+            minHeap.push({x.second,-x.first});
+        }
+        while(minHeap.size()){
+            int freq=minHeap.top().first;
+            int ele=minHeap.top().second;
+            minHeap.pop();
+            res.push_back(-ele);
+            freq--;
+            if(freq!=0) minHeap.push({freq,ele});
+        }
+        return res;
+    }
+};
