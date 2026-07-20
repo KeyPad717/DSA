1a703732d202a8d8129cee01e819e2ff513abbfa
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-20 14:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (11.76%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         vector<int> arr(3,0);
-        if(bills[0]==20)                    arr[2]++;
-        else  if(bills[0]==5)               arr[0]++;
-        else                                arr[1]++;
-        if(bills.size()==1){
-            if(bills[0]==5) return true;
-            return false;
-        }
-        for(int i=1;i<bills.size();i++){
+        // if(bills[0]==20)                    arr[2]++;
+        // else  if(bills[0]==5)               arr[0]++;
+        // else                                arr[1]++;
+        // if(bills.size()==1){
+        //     if(bills[0]==5) return true;
+        //     return false;
+        // }
+        for(int i=0;i<bills.size();i++){
             if(bills[i]==5) arr[0]++;
             else if(bills[i]==10){
                 if(arr[0]>=1){
