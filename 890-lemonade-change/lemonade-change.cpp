50de8c1b2ba2ea5005e67e63228fb0483e873623
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-20 14:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (11.76%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        vector<int> arr(4,0);
-        if(bills[0]==20)    arr[(bills[0]/5)-1]++;
-        else                arr[(bills[0]/5)]++;
+        vector<int> arr(3,0);
+        if(bills[0]==20)                    arr[2]++;
+        else  if(bills[0]==5)               arr[0]++;
+        else                                arr[1]++;
         if(bills.size()==1){
             if(bills[0]==5) return true;
             return false;
         }
         for(int i=1;i<bills.size();i++){
-            if(bills[i]==5) arr[((bills[i])/5)]++;
+            if(bills[i]==5) arr[0]++;
             else if(bills[i]==10){
-                cout<<i<<" "<<bills[i]<<" "<<arr[1]<<endl;
-                if(arr[1]>=1){
-                    arr[1]--;
-                    arr[(bills[i]/5)]++;
+                if(arr[0]>=1){
+                    arr[0]--;
+                    arr[1]++;
                 }
                 else return false;
             }
             else{
-                cout<<i<<" "<<bills[i]<<" "<<endl;
-                if(arr[1]>=1 && arr[2]>=1){
+                if(arr[0]>=1 && arr[1]>=1){
+                    arr[0]--;
                     arr[1]--;
-                    arr[2]--;
-                    arr[(bills[i]/5)-1]++;
+                    arr[2]++;
                 }
-                else if(arr[1]>=3){
-                    arr[1]=arr[1]-3;
-                    arr[(bills[i]/5)-1]++;
+                else if(arr[0]>=3){
+                    arr[0]=arr[0]-3;
+                    arr[2]++;
                 }
                 else    return false;
             }
