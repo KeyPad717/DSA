d8c1f31819f9056c606f1cb1a86601dcd628d923
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-26 19:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (37.62%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    int helper(int stopIdx, int idx, const vector<int>& nums, vector<int> &dp){
-        if(idx==stopIdx)    return nums[idx];
-        if(idx<stopIdx)     return 0;
-        if(dp[idx]!=-1)     return dp[idx];
-        int pick=nums[idx]+helper(stopIdx,idx-2,nums,dp);
-        int not_pick=helper(stopIdx,idx-1,nums,dp);
-        return dp[idx]=max(pick,not_pick);
+    // int helper(int stopIdx, int idx, const vector<int>& nums, vector<int> &dp){
+    //     if(idx==stopIdx)    return nums[idx];
+    //     if(idx<stopIdx)     return 0;
+    //     if(dp[idx]!=-1)     return dp[idx];
+    //     int pick=nums[idx]+helper(stopIdx,idx-2,nums,dp);
+    //     int not_pick=helper(stopIdx,idx-1,nums,dp);
+    //     return dp[idx]=max(pick,not_pick);
+    // }
+    int tabHelper(int start, int end, vector<int>& nums){
+        vector<int> dp(nums.size());
+        dp[start]=nums[start];
+        for(int i=start+1;i<=end;i++){
+            int pick=nums[i];
+            if(i-2>=start)   pick+=dp[i-2];
+            int not_pick=dp[i-1];
+            dp[i]=max(pick,not_pick);
+        }
+        return dp[end];
     }
     int rob(vector<int>& nums) {
         if(nums.size()==1)  return max(0,nums[0]);
         int n=nums.size();
-        vector<int> dp1(nums.size(),-1),dp2(nums.size(),-1);
-        dp1[0]=nums[0];
-        for(int i=1;i<n-1;i++){
-            int pick=0;
-            if(i>1) pick=nums[i]+dp1[i-2];
-            int not_pick=dp1[i-1];
-            dp1[i]=max(pick,not_pick);
-        }
-        dp2[1]=nums[1];
-        for(int i=2;i<n;i++){
-            int pick=0;
-            if(i==2) pick=nums[i];
-            else     pick=nums[i]+dp2[i-2];
-            int not_pick=dp2[i-1];
-            dp2[i]=max(pick,not_pick);
-        }
-        return max(dp1[n-2],dp2[n-1]);
+        return max(tabHelper(0,n-2,nums),tabHelper(1,n-1,nums));
     }
 };
