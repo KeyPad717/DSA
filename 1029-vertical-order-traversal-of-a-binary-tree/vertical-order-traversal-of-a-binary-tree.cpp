e06dd80299dce92c9f0f37fbe9ab260734fb82a5
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-27 14:44:41 +0530

    Time: 3 ms (40.08%) | Memory: 16.5 MB (17.97%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,51 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>> res;
-        map < pair<int,int>, multiset<int> > mp;
-        queue<pair<pair<int,int>,TreeNode*>> q;
-        int row=0, col=0;
-        TreeNode* temp=root;
-        q.push({{row,col},root});
+        vector<vector<int>> ans;
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        map<int,map<int,multiset<int>>> mp;
+        q.push({root,{0,0}});
+        TreeNode* node;
         while(!q.empty()){
-            temp=q.front().second;
-            row=q.front().first.first;
-            col=q.front().first.second;
-            q.pop();
-            mp[{col,row}].insert(temp->val);
-            if(temp->left)  q.push({{row+1,col-1},temp->left});
-            if(temp->right)  q.push({{row+1,col+1},temp->right});
-        }
-        vector<int> v;
-        int prevCol = INT_MIN;
-
-        for(auto &x : mp){
-
-            int currCol = x.first.first;
-
-            if(currCol != prevCol){
-
-                if(!v.empty())
-                    res.push_back(v);
-
-                v.clear();
-                prevCol = currCol;
-            }
-
-            for(int val : x.second){
-                v.push_back(val);
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                node=q.front().first;
+                int x=q.front().second.first;
+                int y=q.front().second.second;
+                q.pop();
+                if(node->left)  q.push({node->left,{x+1,y-1}});
+                if(node->right)  q.push({node->right,{x+1,y+1}});
+                mp[y][x].insert(node->val);
             }
         }
-
-        if(!v.empty())
-            res.push_back(v);
-        return res;
+        for(auto x:mp){
+            vector<int> temp;
+            for(auto y:x.second){
+                temp.insert(temp.end(),y.second.begin(),y.second.end());
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
 };
