5e518d727142d6f3e190e026e59d89191ea7d4ca
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-07-30 22:39:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (45.56%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -12,10 +12,10 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*,long long>> q;
+        queue<pair<TreeNode*, long long>> q;
         TreeNode* node=root;
+        q.push({root,0});
         int maxi=0;
-        q.push({node,0});
         while(!q.empty()){
             int sz=q.size();
             int min_idx=q.front().second;
@@ -23,11 +23,11 @@ public:
             for(int i=0;i<sz;i++){
                 node=q.front().first;
                 int idx=q.front().second-min_idx;
-                if(i==0)  firstIdx=idx;
-                if(i==sz-1) secondIdx=idx;
                 q.pop();
-                if(node->left)  q.push({node->left,2LL*(idx)+1});
-                if(node->right)  q.push({node->right,2LL*(idx)+2});
+                if(i==0)    firstIdx=idx;
+                if(i==sz-1) secondIdx=idx;
+                if(node->left)  q.push({node->left,idx*2LL+1});
+                if(node->right)  q.push({node->right,idx*2LL+2});
             }
             maxi=max(maxi,secondIdx-firstIdx+1);
         }
