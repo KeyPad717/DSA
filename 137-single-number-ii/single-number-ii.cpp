2897b838023bb27f4c314f2e664970947f4b2ca8
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-02 23:28:37 +0530

    Time: 3 ms (31.42%) | Memory: 13.3 MB (65.46%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int ele=0;
+        for(int i=0;i<=31;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if((nums[j])&(1<<i))    cnt++;
+            }
+            if(cnt%3)  ele+=(1<<i)*1;
+            else        ele+=(1<<i)*0;
+        }
+        return ele;
+    }
+};
