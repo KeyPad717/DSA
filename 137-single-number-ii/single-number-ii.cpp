91018774cd740726355a0847c9ff50f3032f20d6
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-09 10:41:31 +0530

    Time: 6 ms (5.15%) | Memory: 13.4 MB (66.15%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i+=3){
-            if(nums[i]!=nums[i-1])  return nums[i-1];
+        int n=nums.size(), num=0;
+        for(int i=0;i<=31;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if((1<<i)&(nums[j]))    cnt++;
+            }
+            cout<<i<<" "<<cnt<<endl;
+            if(cnt%3)   num+=(1<<i);
         }
-        return nums[n-1];
+        return num;
     }
 };
