7ace1a7dd909d6cdd48544affd328e84ad870876
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-11 16:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (95.16%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string str="", ans="";
-        int j=0;
-        while(j<s.size()){
-            if(s[j]==' '){
-                while(j<s.size() && s[j]==' '){
-                    j++;
-                }
-                if(ans=="") ans=str;
-                else {
-                    str.push_back(' ');
-                    ans.insert(0,str);
-                }
-                str="";
-            }
-            else{
-                str.push_back(s[j]);
-                j++;
+        // 1. Reverse the entire string
+        reverse(s.begin(), s.end());
+
+        int n = s.size();
+        int i = 0;
+
+        // 2. Reverse each word
+        for (int j = 0; j <= n; j++) {
+            if (j == n || s[j] == ' ') {
+                reverse(s.begin() + i, s.begin() + j);
+                i = j + 1;
             }
         }
-        if(str!=""){
-            str.push_back(' ');
-            ans.insert(0,str);
+
+        // 3. Remove extra spaces
+        int write = 0;
+
+        for (int read = 0; read < n; read++) {
+            if (s[read] != ' ' ||
+                (write > 0 && s[write - 1] != ' ')) {
+                s[write++] = s[read];
+            }
         }
-        if(ans!="" && ans[ans.size()-1]==' ')  ans.pop_back();
-        return ans;
+
+        // Remove trailing space
+        if (write > 0 && s[write - 1] == ' ')
+            write--;
+
+        s.resize(write);
+
+        return s;
     }
 };
