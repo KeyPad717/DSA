c23ed7cceaf4cca45804111666c8a915a4666068
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-13 19:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (7.73%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size()!=t.size())  return false;
-        unordered_map<char,char> mp1;
-        unordered_map<char,int> mp2;
-        string x="";
-        int i=0;
-        while(i<s.size()){
-            if(mp1.find(s[i])==mp1.end()){
-                if(mp2.find(t[i])!=mp2.end())   return false;
-                mp1[s[i]]=t[i];
-                mp2[t[i]]++;
-            }
-            x.push_back(mp1[s[i]]);
-            i++;
+    if (s.size() != t.size())
+        return false;
+
+    unordered_map<char, char> mp1;
+    unordered_map<char, int> mp2;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (mp1.find(s[i]) == mp1.end()) {
+            if (mp2.find(t[i]) != mp2.end())
+                return false;
+            mp1[s[i]] = t[i];
+            mp2[t[i]]++;
         }
-        return x==t;
+        else{
+            if(t[i]!=mp1[s[i]]) return false;
+        }
     }
+
+    return true;
+}
     // bool isIsomorphic(string s, string t) {
     //     if(s.size()!=t.size())  return false;
     //     map<char,char> mp1;
