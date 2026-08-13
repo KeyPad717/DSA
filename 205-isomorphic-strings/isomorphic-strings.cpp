be9a812eb7b65243bd084f5d76894921ebcbecdd
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-13 19:07:26 +0530

    Time: 3 ms (40.91%) | Memory: 9.6 MB (7.73%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if(s.size()!=t.size())  return false;
-        map<char,char> mp1;
-        map<char,int> mp2;
+        unordered_map<char,char> mp1;
+        unordered_map<char,int> mp2;
         string x="";
         int i=0;
         while(i<s.size()){
-            // if(s[i]==t[i]){
-            //     x.push_back(s[i]);
-            //     i++;
-            //     continue;
-            // }
             if(mp1.find(s[i])==mp1.end()){
                 if(mp2.find(t[i])!=mp2.end())   return false;
                 mp1[s[i]]=t[i];
@@ -22,4 +17,21 @@ public:
         }
         return x==t;
     }
+    // bool isIsomorphic(string s, string t) {
+    //     if(s.size()!=t.size())  return false;
+    //     map<char,char> mp1;
+    //     vector<int> mp2(256,0);
+    //     string x="";
+    //     int i=0;
+    //     while(i<s.size()){
+    //         if(mp1.find(s[i])==mp1.end()){
+    //             if(mp2[t[i]]!=0)   return false;
+    //             mp1[s[i]]=t[i];
+    //             mp2[t[i]-'0']++;
+    //         }
+    //         x.push_back(mp1[s[i]]);
+    //         i++;
+    //     }
+    //     return x==t;
+    // }
 };
