448fb7934aabd23bf8a96fcd801ce855bef9326c
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-15 23:56:04 +0530

    Time: 422 ms (26.39%) | Memory: 296.8 MB (6.89%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    int helper(int idx, vector<int>& nums, int prevIdx, vector<vector<int>>& dp){
-        if(idx==nums.size()){
-            return 0;
-        }
-        if(dp[idx][prevIdx]!=-1)    return dp[idx][prevIdx];
-        int take=0;
-        if(prevIdx==nums.size()){
-            take=1+helper(idx+1,nums,idx,dp);
-        }
-        else if(nums[idx]>nums[prevIdx]){
-            take=1+helper(idx+1,nums,idx,dp);
-        }
-        return dp[idx][prevIdx]=max(take,helper(idx+1,nums,prevIdx,dp));
-    }
+    // int helper(int idx, vector<int>& nums, int prevIdx, vector<vector<int>>& dp){
+    //     if(idx==nums.size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx][prevIdx]!=-1)    return dp[idx][prevIdx];
+    //     int take=0;
+    //     if(prevIdx==nums.size()){
+    //         take=1+helper(idx+1,nums,idx,dp);
+    //     }
+    //     else if(nums[idx]>nums[prevIdx]){
+    //         take=1+helper(idx+1,nums,idx,dp);
+    //     }
+    //     return dp[idx][prevIdx]=max(take,helper(idx+1,nums,prevIdx,dp));
+    // }
     int lengthOfLIS(vector<int>& nums) { 
         int n=nums.size();
         vector<vector<int>> dp(n+1,vector<int> (n+1));
+        vector<int> prev(n+1), curr(n+1);
         for(int pidx=0;pidx<=n;pidx++){
-            dp[n][pidx]=0;
+            curr[pidx]=0;
         }
         for(int idx=n-1;idx>=0;idx--){
             for(int pidx=0;pidx<=n;pidx++){
                 int take=0;
                 if(pidx==n || nums[idx]>nums[pidx]){
-                    take=1+dp[idx+1][idx];
+                    take=1+curr[idx];
                 }
-                dp[idx][pidx]=max(take, dp[idx+1][pidx]);
+                prev[pidx]=max(take, curr[pidx]);
             }
+            curr=prev;
         }
-        return dp[0][n];
+        return prev[n];
     }
 };
