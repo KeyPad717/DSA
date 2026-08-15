06bfaa021105bc6c75cc4848a7e8cccddbba647b
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-16 00:31:13 +0530

    Time: 72 ms (51.94%) | Memory: 14.5 MB (49.03%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
-    // int helper(int idx, vector<int>& nums, int prevIdx, vector<vector<int>>& dp){
-    //     if(idx==nums.size()){
-    //         return 0;
-    //     }
-    //     if(dp[idx][prevIdx]!=-1)    return dp[idx][prevIdx];
-    //     int take=0;
-    //     if(prevIdx==nums.size()){
-    //         take=1+helper(idx+1,nums,idx,dp);
-    //     }
-    //     else if(nums[idx]>nums[prevIdx]){
-    //         take=1+helper(idx+1,nums,idx,dp);
-    //     }
-    //     return dp[idx][prevIdx]=max(take,helper(idx+1,nums,prevIdx,dp));
-    // }
     int lengthOfLIS(vector<int>& nums) { 
-        int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1));
-        vector<int> next(n+1), curr(n+1);
-        for(int pidx=0;pidx<=n;pidx++){
-            next[pidx]=0;
-        }
-        for(int idx=n-1;idx>=0;idx--){
-            for(int pidx=0;pidx<=n;pidx++){
-                int take=0;
-                if(pidx==n || nums[idx]>nums[pidx]){
-                    take=1+next[idx];
-                }
-                curr[pidx]=max(take, next[pidx]);
+        int maxi=1,n=nums.size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i])    dp[i]=max(dp[i],1+dp[j]);
             }
-            next=curr;
         }
-        return curr[n];
+        return (*max_element(dp.begin(),dp.end()));
     }
 };
