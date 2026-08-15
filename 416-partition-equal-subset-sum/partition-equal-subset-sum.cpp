ab35e3458241dfacfa292cfa2706cb95845d044a
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-15 14:12:05 +0530

    Time: 167 ms (25.46%) | Memory: 17.4 MB (60.54%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    bool helper(int idx, vector<int>& nums, vector<vector<int>>& dp, int sum){
-        if(sum==0)  return true;
-        if(idx<0 || sum<0)  return false;
-        if(dp[idx][sum]!=-1)  return dp[idx][sum]==1;
-        bool pick=false;
-        if(nums[idx]<=sum){
-            pick=helper(idx-1, nums, dp, sum-nums[idx]);
-        }
-        dp[idx][sum]=(pick || helper(idx-1, nums, dp, sum));
-        return dp[idx][sum]==1;
-    }
     bool canPartition(vector<int>& nums) {
         int sum=0;
         for(int x:nums){
@@ -21,7 +10,6 @@ public:
         vector<vector<bool>> dp(n, vector<bool> (targetSum+1));
         for(int s=0;s<=targetSum;s++){
             if(nums[0]==s)   dp[0][s]=true;
-            else            dp[0][s]=false;
         }
         for(int idx=0;idx<n;idx++){
             dp[idx][0]=true;
@@ -31,10 +19,8 @@ public:
                 bool pick=false;
                 if(nums[idx]<=s){
                     pick=dp[idx-1][s-nums[idx]];
-                    //helper(idx-1, nums, dp, sum-nums[idx]);
                 }
                 dp[idx][s]=(pick || dp[idx-1][s]);
-                //helper(idx-1, nums, dp, sum));
             }
         }
         return dp[n-1][targetSum];
