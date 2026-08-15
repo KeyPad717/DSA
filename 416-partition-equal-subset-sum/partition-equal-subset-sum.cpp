9ab98195320dcaeacdbd4f70f2c037046e88a108
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-15 13:45:12 +0530

    Time: 43 ms (94.75%) | Memory: 110.6 MB (24.43%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    bool solve(int n, int k, vector<int>& nums, vector<vector<int>>& dp){
-        if(k==0)    return true;
-        if(n==0)    return false;
-        if(dp[n][k]!=-1)    return dp[n][k];
-        if(nums[n-1]<=k){
-            dp[n][k]=solve(n-1,k-nums[n-1],nums,dp)||solve(n-1,k,nums,dp);
-            return dp[n][k];
+    bool helper(int idx, vector<int>& nums, vector<vector<int>>& dp, int sum){
+        if(sum==0)  return true;
+        if(idx<0 || sum<0)  return false;
+        if(dp[idx][sum]!=-1)  return dp[idx][sum]==1;
+        int pick=false;
+        if(nums[idx]<=sum){
+            pick=helper(idx-1, nums, dp, sum-nums[idx]);
         }
-        dp[n][k]=solve(n-1,k,nums,dp);
-        return dp[n][k];
+        dp[idx][sum]=(pick || helper(idx-1, nums, dp, sum));
+        return dp[idx][sum]==1;
     }
     bool canPartition(vector<int>& nums) {
-        int x=accumulate(nums.begin(),nums.end(),0);
-        if(x%2!=0)  return false;
-        int n=nums.size(),k=x/2;
-        vector<vector<int>> dp(n+1,vector<int> (k+1,-1));
-        return(solve(n,k,nums,dp));
+        int sum=0;
+        for(int x:nums){
+            sum+=x;
+        }
+        if(sum&1)   return false;
+        int n=nums.size();
+        vector<vector<int>> dp(n, vector<int> ((sum/2)+1,-1));
+        return helper(n-1, nums, dp, sum/2);
     }
 };
