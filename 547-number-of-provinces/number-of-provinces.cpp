513b1481028ff7216fd9c26ac060e28bdeec372b
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-08-18 18:15:24 +0530

    Time: 2 ms (40.90%) | Memory: 20.5 MB (30.04%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,55 +1,30 @@
-class DisjointSet{
-    public:
-        vector<int> rank, parent;
-        DisjointSet(int n){
-            rank.resize(n+1,0);
-            parent.resize(n+1);
-            for(int i=0;i<=n;i++){
-                parent[i]=i;
-            }
-        }
-        int findUpar(int node){
-            if(node==parent[node])  return node;
-            parent[node]=findUpar(parent[node]);
-            return parent[node];
-        }
-        bool unionByRank(int u, int v){
-            int ult_u=findUpar(u);
-            int ult_v=findUpar(v);
-            if(ult_u==ult_v)    return false;
-            if(rank[ult_u]<rank[ult_v]){
-                parent[ult_u]=ult_v;
-            }
-            else if(rank[ult_v]<rank[ult_u]){
-                parent[ult_v]=ult_u;
-            }
-            else{
-                parent[ult_v]=ult_u;
-                rank[ult_u]++;
-            }
-            return true;
-        } 
-};
 class Solution {
 public:
+    void dfs(int node, const vector<vector<int>>& adj, vector<int>& vis){
+        vis[node]=1;
+        for(int j:adj[node]){
+            if(vis[j]==0){
+                dfs(j,adj,vis);
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        DisjointSet ds(n);
-        //vector<pair<int,int>> edges;
+        int n=isConnected.size(), prov=0;
+        vector<vector<int>> adj(n);
+        vector<int> vis(n,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j && isConnected[i][j]==1){
-                    ds.unionByRank(i,j);
+                    adj[i].push_back(j);
                 }
             }
         }
-        int count=0;
         for(int i=0;i<n;i++){
-            ds.findUpar(i);
+            if(vis[i]==0){
+                prov++;
+                dfs(i,adj,vis);
+            }
         }
-        for(int i=0;i<n;i++){
-            if(i==ds.parent[i])  count++;
-        }
-        return count;
+        return prov;
     }
 };
