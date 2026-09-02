1b2337c1506e7f9117eb7b45f8576790460379a3
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-09-02 18:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (39.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
         int n1=nums1.size(), n2=nums2.size();
-        unordered_map<int,int> mp;
-        vector<int> res(n1);
+        vector<int> nge1(n1);
+        unordered_map<int,int> ngePair;
+        stack<int> st;
         for(int i=n2-1;i>=0;i--){
             if(st.empty()){
-                mp[nums2[i]]=-1;
                 st.push(nums2[i]);
-            } 
+                ngePair[nums2[i]]=-1;
+            }  
             else{
-                while(!st.empty()){
-                    if(st.top()<nums2[i])
-                        st.pop();
-                    else break;
-                }
+                while(!st.empty() && st.top()<=nums2[i])    st.pop();
                 if(st.empty()){
-                    mp[nums2[i]]=-1;
                     st.push(nums2[i]);
-                } 
+                    ngePair[nums2[i]]=-1;
+                }  
                 else{
-                    mp[nums2[i]]=st.top();
+                    ngePair[nums2[i]]=st.top();
                     st.push(nums2[i]);
                 }
             }
         }
         for(int i=0;i<n1;i++){
-            res[i]=mp[nums1[i]];
+            nge1[i]=ngePair[nums1[i]];
         }
-        return res;
+        return nge1;
     }
 };
