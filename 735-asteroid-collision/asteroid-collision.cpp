b792b308b0cdc974a7d151a649f013902acda376
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-09-03 22:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (41.06%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -3,30 +3,12 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
         vector<int> ans;
-        for( int x: asteroids){
-            if(st.empty())  st.push(x);
+        for(int x: asteroids){
+            if(st.empty() || x>0)  st.push(x);
             else{
-                if(x>0){
-                    st.push(x);
-                }
-                else{
-                    int f=0;
-                    while(!st.empty() && st.top()>0){
-                        if(st.top()==abs(x)){
-                            f++;
-                            st.pop();
-                            break;
-                        }
-                        else if(abs(st.top())<abs(x))  st.pop();   
-                        else{
-                            break;
-                        }                   
-                    }
-                    if(st.empty() && f==0)  st.push(x);
-                    else if(f==0){
-                        if(st.top()<0)  st.push(x);
-                    }
-                }
+                while(!st.empty() && st.top()>0 && st.top()<abs(x))     st.pop();
+                if(!st.empty() && st.top()==abs(x))     st.pop();
+                else if(st.empty() || st.top()<0)  st.push(x);
             }
         }
         while(!st.empty()){
