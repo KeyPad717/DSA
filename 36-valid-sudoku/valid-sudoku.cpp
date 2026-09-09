310fab603b766565af103bc9c9ed8a8df9bff535
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-09-09 23:29:56 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (86.10%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<char,int> mp;
+        vector<bool> seen(10,false);
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
+                    if(seen[board[i][j]-'0']==false){
+                        seen[board[i][j]-'0']=true;
                     }
                     else    return false;
                 }
             }
-            mp.clear();
+            fill(seen.begin(), seen.end(), false);
         }
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[j][i]!='.'){
-                    if(mp.find(board[j][i])==mp.end()){
-                        mp[board[j][i]]++;
+                    if(seen[board[j][i]-'0']==false){
+                        seen[board[j][i]-'0']=true;
                     }
                     else    return false;
                 }
             }
-            mp.clear();
+            fill(seen.begin(), seen.end(), false);
         }
         for(int br = 0; br < 9; br++) {
             int startRow = (br / 3) * 3;
@@ -30,14 +30,14 @@ public:
             for(int i=startRow;i<startRow+3;i++){
                 for(int j=startCol;j<startCol+3;j++){
                     if(board[i][j]!='.'){
-                        if(mp.find(board[i][j])==mp.end()){
-                            mp[board[i][j]]++;
+                        if(seen[board[i][j]-'0']==false){
+                            seen[board[i][j]-'0']=true;
                         }
                         else    return false;
                     }
                 }
             }
-            mp.clear();
+            fill(seen.begin(), seen.end(), false);
         }        
         return true;
     }
