fac708af2b4522baa4a4326df851dfc9eb6fad66
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-09-09 23:24:49 +0530

    Time: 10 ms (13.56%) | Memory: 22.6 MB (56.00%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -24,105 +24,21 @@ public:
             }
             mp.clear();
         }
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
+        for(int br = 0; br < 9; br++) {
+            int startRow = (br / 3) * 3;
+            int startCol = (br % 3) * 3;
+            for(int i=startRow;i<startRow+3;i++){
+                for(int j=startCol;j<startCol+3;j++){
+                    if(board[i][j]!='.'){
+                        if(mp.find(board[i][j])==mp.end()){
+                            mp[board[i][j]]++;
+                        }
+                        else    return false;
                     }
-                    else    return false;
                 }
             }
-        }
-        mp.clear();
-        for(int i=3;i<6;i++){
-            for(int j=0;j<3;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
-        for(int i=6;i<9;i++){
-            for(int j=0;j<3;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
-        for(int i=0;i<3;i++){
-            for(int j=3;j<6;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
-        for(int i=3;i<6;i++){
-            for(int j=3;j<6;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
-        for(int i=6;i<9;i++){
-            for(int j=3;j<6;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
-        for(int i=0;i<3;i++){
-            for(int j=6;j<9;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
-        for(int i=3;i<6;i++){
-            for(int j=6;j<9;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
-        for(int i=6;i<9;i++){
-            for(int j=6;j<9;j++){
-                if(board[i][j]!='.'){
-                    if(mp.find(board[i][j])==mp.end()){
-                        mp[board[i][j]]++;
-                    }
-                    else    return false;
-                }
-            }
-        }
-        mp.clear();
+            mp.clear();
+        }        
         return true;
     }
 };
