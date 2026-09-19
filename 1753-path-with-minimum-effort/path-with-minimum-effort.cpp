cfc2171ae85dec82f65d45f6812d6eb308ac118b
Keyur Sanjaykumar Padiya <77205289+KeyPad717@users.noreply.github.com>
2026-09-20 03:00:14 +0530

    Time: 42 ms (87.34%) | Memory: 25.3 MB (40.15%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int dr[4]={0,0,1,-1};
-    int dc[4]={-1,1,0,0};
+    int dx[4]={0,0,-1,1};
+    int dy[4]={1,-1,0,0};
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int r=heights.size(), c=heights[0].size(), maxi=INT_MIN;
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, 
-                        greater<pair<int,pair<int,int>>>> q;
-        vector<vector<int>> dist(r, vector<int> (c,INT_MAX));
-        q.push({0,{0,0}});  
-        dist[0][0]=0;
-        while(!q.empty()){
-            int d=q.top().first;
-            int x=q.top().second.first;
-            int y=q.top().second.second;
-            q.pop();
-            if(x==r-1 && y==c-1){
-                return d;
+        int rows=heights.size(), cols=heights[0].size();
+        vector<vector<int>> maxPath(rows, vector<int> (cols,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> minHeap;
+        maxPath[0][0]=0;
+        minHeap.push({0,{0,0}});
+        while(!minHeap.empty()){
+            int x=minHeap.top().second.first;
+            int y=minHeap.top().second.second;
+            int curDiff=minHeap.top().first;
+            if(x==rows-1 && y==cols-1){
+                return curDiff;
             }
-            //if(d!=dist[x][y])   continue;
+            minHeap.pop();
             for(int i=0;i<4;i++){
-                int c1=dr[i]+x;
-                int c2=dc[i]+y;
-                if(c1>=0 && c1<r && c2>=0 && c2<c){
-                    int newEff=max(d, abs(heights[c1][c2]-heights[x][y]));
-                    if(newEff<dist[c1][c2]){
-                        dist[c1][c2]=newEff;
-                        q.push({dist[c1][c2],{c1,c2}});
+                int c1=x+dx[i];
+                int c2=y+dy[i];
+                if(c1>=0 && c2>=0 && c1<rows && c2<cols){
+                    int newEff=max(abs(heights[c1][c2]-heights[x][y]), curDiff);
+                    if(curDiff<maxPath[c1][c2]){
+                        maxPath[c1][c2]=newEff;
+                        minHeap.push({newEff,{c1,c2}});
                     }
                 }
             }
         }
-        return -1;
+        return 0;
     }
 };
